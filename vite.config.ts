50ca392a6aa84cb39f81a807318c6e64c8b9d40e
Josue Giron <benjamingiron1@gmail.com>
2025-08-31 01:14:23 -0600

    fix: fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Reemplaza "mi-repo" por el nombre exacto del repo si vas a GitHub Pages.
 export default defineConfig({
   plugins: [react()],
+  // solo si tu index.html NO está en la raíz:
+  root: "frontend",
   base: process.env.GITHUB_PAGES ? "/catalogo-productos/" : "/",
+  build: { outDir: "dist", emptyOutDir: true }
 });
