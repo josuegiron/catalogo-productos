f361a9b7fa7e2cd391dfaa4de77def054bfd5ff8
Josue Giron <benjamingiron1@gmail.com>
2025-08-31 01:22:36 -0600

    feat: update

---
+++ tsconfig.ts
@@ -0,0 +1,13 @@
+{
+  "compilerOptions": {
+    "jsx": "react-jsx",
+    "target": "ES2020",
+    "module": "ESNext",
+    "moduleResolution": "Bundler",
+    "lib": ["ES2020", "DOM"],
+    "strict": true,
+    "esModuleInterop": true,
+    "skipLibCheck": true
+  },
+  "include": ["src"]
+}
