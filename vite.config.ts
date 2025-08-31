8edcaf75b326c16c4df72984ea33d346d1672ffc
Josue Giron <benjamingiron1@gmail.com>
2025-08-31 01:25:51 -0600

    chore: enable GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // solo si tu index.html NO está en la raíz:
   base: process.env.GITHUB_PAGES ? "/catalogo-productos/" : "/",
-  build: { outDir: "dist", emptyOutDir: true }
 });
